c9340637c3e5157d2e542f2b7e2f63b163d8ab64
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-06-09 21:55:49 -0300

    criacao da funcao que checa o cep e chamada das funcoes importadas que imprimem o erro

--- modules/regexInputs.js
+++ modules/regexInputs.js
@@ -1,12 +1,28 @@
-const name = document.getElementById('name');
+import {errorName, errorCep} from './printError';
 
+const name = document.getElementById('name');
+const cep = document.getElementById('cep');
+
+let condicional = Boolean;
 
 function checkName() {
     const reg = /[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]/g;
     let nameValue = name.value;
 
-    return nameValue.match(reg) !== null ? true : false;
+    condicional = nameValue.match(reg) !== null ? true : false;
+
+    if (condicional === false) errorName() 
+}
+
+function checkCep() {
+    const reg = /^[0-9]{5}-[0-9]{3}$/g;
+    let cepValue = cep.value;
+
+    condicional = cepValue.match(reg) !== null ? true : false;
+
+    if (condicional === false) errorCep() 
+
 }
 
 
-export default checkName
+export {checkName, checkCep}
