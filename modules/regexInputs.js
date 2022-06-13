e39752507bc59bd294bcb5297686acbeb332a191
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-06-13 20:09:46 -0300

    arrumando regex de cep, e adicionando um comentario

--- modules/regexInputs.js
+++ modules/regexInputs.js
@@ -1,4 +1,4 @@
-import {errorName, errorCep} from './printError';
+import {errorName, errorCep} from './printError.js';
 
 const name = document.getElementById('name');
 const cep = document.getElementById('cep');
@@ -6,22 +6,22 @@ const cep = document.getElementById('cep');
 let condicional = Boolean;
 
 function checkName() {
-    const reg = /[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]/g;
+    const reg = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/g;
     let nameValue = name.value;
 
     condicional = nameValue.match(reg) !== null ? true : false;
+    //o metodo match retorna null caso o valor do input contenha algum caracter fora da regexp 
 
-    if (condicional === false) errorName() 
+    if (condicional === false) errorName();
 }
 
 function checkCep() {
-    const reg = /^[0-9]{5}-[0-9]{3}$/g;
+    const reg = /^([\d]{2})\.?([\d]{3})\-?([\d]{3})/g;
     let cepValue = cep.value;
 
     condicional = cepValue.match(reg) !== null ? true : false;
 
-    if (condicional === false) errorCep() 
-
+    if (condicional === false) errorCep();
 }
 
 
