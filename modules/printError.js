b4fa0a1f52ae0f1359674843d5917045ebd6ed76
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-06-13 20:10:36 -0300

    adicao das mensagens para cada erro e criacao da funcao que limpa os textos e por fim importacao de todas as funcoes

--- modules/printError.js
+++ modules/printError.js
@@ -1,1 +1,17 @@
+const msgName = document.getElementById('paragraph-name')
+const msgCep = document.getElementById('paragraph-cep')
 
+function errorName() {
+    msgName.innerText = "Nome inválido"
+}
+
+function errorCep() {
+    msgCep.innerText += "Cep inválido"
+}
+
+function clearMsgError() {
+    msgName.innerText = "";
+    msgCep.innerText = "";
+}
+
+export { errorName, errorCep, clearMsgError }
