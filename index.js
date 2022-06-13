b62baae1838aec0217e1864f8d27ca6b219ef380
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-06-13 20:08:47 -0300

    importacao da funcao que limpa a mensagem de erro a cada clique no botao

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import {checkName, checkCep} from './modules/regexInputs.js';
+import {clearMsgError} from './modules/printError.js'
 
 const submit = document.getElementById('button');
 
 submit.addEventListener('click', evt => {
     evt.preventDefault();
+    clearMsgError()
 
     checkForm()
 })
 
 function checkForm () {
-
-    
-    
-
+    checkName();
+    checkCep();
 }
