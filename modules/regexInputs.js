ce6194d673a185fb398fdbe198578f9beecd6957
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-07-06 21:39:16 -0300

    funcao checkCep setada como default na exportacao e exclusao de funcao que checava o name

--- modules/regexInputs.js
+++ modules/regexInputs.js
@@ -1,19 +1,8 @@
 import searchCep from './validateCep.js';
 
-const name = document.getElementById('name');
 const cep = document.getElementById('cep');
 
 let condicional = Boolean;
-
-function checkName() {
-    const reg = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/g;
-    let nameValue = name.value;
-
-    condicional = nameValue.match(reg) !== null ? true : false;
-    //o metodo match retorna null caso o valor do input contenha algum caracter fora da regexp 
-
-    return (condicional === true) ? true : false;
-}
 
 function checkCep() {
     const reg = /^([\d]{2})\.?([\d]{3})\-?([\d]{3})/g;
@@ -27,4 +16,4 @@ function checkCep() {
 }
 
 
-export {checkName, checkCep}
+export default checkCep
