9217590183a6f5d68138c06f09990fb922f01f74
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-07-06 21:38:24 -0300

    remocao de funcao que exibia mensagem de erro no input name

--- modules/printError.js
+++ modules/printError.js
@@ -1,17 +1,11 @@
-const msgName = document.getElementById('paragraph-name')
 const msgCep = document.getElementById('paragraph-cep')
-
-function errorName() {
-    msgName.innerText = "Nome inválido"
-}
 
 function errorCep() {
     msgCep.innerText += "Cep inválido"
 }
 
 function clearMsgError() {
-    msgName.innerText = "";
     msgCep.innerText = "";
 }
 
-export { errorName, errorCep, clearMsgError }
+export { errorCep, clearMsgError }
