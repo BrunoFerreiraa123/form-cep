2dcc9bbfa8a976225eea02bd64bba4118cdfe5ab
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-07-12 21:42:34 -0300

    nome do arquivo alterado de validateCep para searchCep, pois a funcao do arquivo e buscar o endereco do cep

---
+++ modules/searchCep.js
@@ -0,0 +1,28 @@
+import { errorCep, clearMsgCep } from './printError.js'
+
+const fillForm = (endereco) => {
+    document.getElementById('street').value = endereco.logradouro;
+    document.getElementById('city').value = endereco.localidade;
+    document.getElementById('district').value = endereco.bairro;
+    document.getElementById('uf').value = endereco.uf;
+}
+
+const searchCep = async () => {
+    clearMsgCep();
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+
+    if (endereco.hasOwnProperty('erro')) {
+        errorCep();
+    } else {
+        fillForm(endereco)
+    }
+}
+
+document.getElementById('cep')
+    .addEventListener("focusout", searchCep);
+
+export default searchCep;
